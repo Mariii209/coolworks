82d659ce739cebfa5d5510a1f2a56e01c4ea40b6
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-08-07 10:49:26 -0700

    Create ThermoIcon component as reusable SVG for use in other components

---
+++ src/SvgIcons/ThermoIcon.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+export default function ThermoIcon() {
+  return (
+    <div className="ThermoIcon">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="28"
+        height="29"
+        viewBox="0 0 28 29"
+        fill="none"
+      >
+        <path
+          d="M14 10.75C12.959 10.7332 11.9423 11.065 11.1116 11.6927C10.2809 12.3203 9.68398 13.2077 9.41572 14.2136C9.14746 15.2196 9.2233 16.2864 9.63118 17.2443C10.0391 18.2022 10.7556 18.9962 11.6667 19.5"
+          stroke="#E7000B"
+          stroke-width="2.33333"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M14 3.75V6.08333"
+          stroke="#E7000B"
+          stroke-width="2.33333"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M7.70001 21.7167L6.06668 23.35"
+          stroke="#E7000B"
+          stroke-width="2.33333"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M23.3333 4.91671V17.2134C24.223 17.727 24.9183 18.5199 25.3115 19.469C25.7046 20.4181 25.7735 21.4704 25.5077 22.4626C25.2418 23.4549 24.6559 24.3318 23.8409 24.9571C23.0259 25.5825 22.0273 25.9215 21 25.9215C19.9727 25.9215 18.9741 25.5825 18.1591 24.9571C17.3441 24.3318 16.7582 23.4549 16.4924 22.4626C16.2265 21.4704 16.2954 20.4181 16.6886 19.469C17.0817 18.5199 17.777 17.727 18.6667 17.2134V4.91671C18.6667 4.29787 18.9125 3.70438 19.3501 3.26679C19.7877 2.82921 20.3812 2.58337 21 2.58337C21.6189 2.58337 22.2123 2.82921 22.6499 3.26679C23.0875 3.70438 23.3333 4.29787 23.3333 4.91671Z"
+          stroke="#E7000B"
+          stroke-width="2.33333"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M4.66668 15.4166H2.33334"
+          stroke="#E7000B"
+          stroke-width="2.33333"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M7.39668 8.81334L5.75168 7.16833"
+          stroke="#E7000B"
+          stroke-width="2.33333"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </div>
+  );
+}
