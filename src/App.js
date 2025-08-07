9c18f72b84eaf749262a80f2158f1f99c8eda148
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-08-07 10:49:38 -0700

    Import Service and Professional components into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import HVACHeroSection from "./HVACHeroSection";
+import Services from "./Services";
+import Professional from "./Professional";
 
 function App() {
   return (
     <div className="App">
       <HVACHeroSection />
+      <Services />
+      <Professional />
     </div>
   );
 }
